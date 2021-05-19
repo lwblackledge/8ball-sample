2bd2945194a7a83fa920d386025ce958f9f5dd30
Laurence Blackledge <lwblackledge@gmail.com>
2021-05-19 13:44:29 -0400

    Clean

--- src/main/java/com/example/eightball/controller/AnswerController.java
+++ src/main/java/com/example/eightball/controller/AnswerController.java
@@ -2,13 +2,7 @@ package com.example.eightball.controller;
 
 import com.example.eightball.service.AnswerService;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/answer")
 public class AnswerController {
 
   @NonNull
--- src/main/java/com/example/eightball/service/AnswerService.java
+++ src/main/java/com/example/eightball/service/AnswerService.java
@@ -2,13 +2,11 @@ package com.example.eightball.service;
 
 import com.example.eightball.configuration.EightBallConfig;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class AnswerService {
 
